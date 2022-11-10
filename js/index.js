93280e196d2757709177a05ef3bfb7ee3fd562be
justintsugranes <justintsugranes@proton.me>
2022-11-10 11:28:24 -0500

    edit contact form

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 import bootstrap from 'bootstrap';
-import Lightbox from 'bs5-lightbox';
+// import Lightbox from 'bs5-lightbox';
 
-const options = {
-  keyboard: true,
-  size: 'fullscreen',
-};
+// const options = {
+//   keyboard: true,
+//   size: 'fullscreen',
+// };
 
-document.querySelectorAll('.my-lightbox-toggle').forEach((el) =>
-  el.addEventListener('click', (e) => {
-    e.preventDefault();
-    const lightbox = new Lightbox(el, options);
-    lightbox.show();
-  })
-);
+// document.querySelectorAll('.my-lightbox-toggle').forEach((el) =>
+//   el.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const lightbox = new Lightbox(el, options);
+//     lightbox.show();
+//   })
+// );
+
+$('#myForm').on('submit', function (e) {
+  $('#success').modal('show');
+  e.preventDefault();
+});
