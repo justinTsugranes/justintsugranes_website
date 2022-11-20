4fa9d280f081d6c1453fb25dcb88b2f7e9eda470
justintsugranes <justintsugranes@proton.me>
2022-11-20 11:17:07 -0500

    remove unnecessary import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import userData from '../constants/data.js';
 import LinkedInIcon from './icons/LinkedInIcon.js';
