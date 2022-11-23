b035f060b4c5315ebcb997a5163ec867c6ed428d
justintsugranes <justintsugranes@proton.me>
2022-11-23 13:53:21 -0500

    add type animation

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Hero.css';
 import { Row, Container, Col } from 'react-bootstrap';
 import heroImage from '../img/heros/hero-index.png';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Hero() {
   return (
@@ -9,10 +10,35 @@ export default function Hero() {
       <Col>
         <Row className="color-overlay d-flex justify-content-center align-items-center h-100">
           <Col className="text-white text-center">
-            <h1 className="mb-3">Web Developer, Digital Media Creator</h1>
-            <h4 className="mb-3 light-blue">
+            {/* <h1 className="mb-3">Web Developer, Digital Media Creator</h1> */}
+            <TypeAnimation
+              sequence={[
+                'Web Developer, Digital Media Creator',
+                () => {
+                  console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                }
+              ]}
+              wrapper="div"
+              cursor={false}
+              // repeat={Infinity}
+              style={{ fontSize: '4em' }}
+            />
+            {/* <h4 className="mb-3 light-blue">
               JavaScript - React.js - Bootstrap - Adobe Creative Cloud - Figma
-            </h4>
+            </h4> */}
+            <TypeAnimation
+              sequence={[
+                2500,
+                'JavaScript - React.js - Bootstrap - Adobe Creative Cloud - Figma',
+                () => {
+                  console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                }
+              ]}
+              wrapper="div"
+              cursor={false}
+              // repeat={Infinity}
+              style={{ fontSize: '2em' }}
+            />
           </Col>
         </Row>
       </Col>
