f53c01eb883ce2f1cf0876b96903d3a765601f0b
justintsugranes <justintsugranes@proton.me>
2022-12-29 06:59:16 -0500

    test new form

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,71 +1,81 @@
 import { useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
-import { Modal, CloseButton, Container, Form, Button, FloatingLabel } from 'react-bootstrap';
+import { Modal, CloseButton, Container, Form, Button /* FloatingLabel  */ } from 'react-bootstrap';
 
-const ContactModal = () => {
-  /* show/hide the contact modal */
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  // form validation
-  const [validated, setValidated] = useState(false);
-  // success honeypot
-  const [success, setSuccess] = useState(false);
+// const ContactModal = () => {
+/* show/hide the contact modal */
+// const [show, setShow] = useState(false);
+// const handleClose = () => setShow(false);
+// const handleShow = () => setShow(true);
+// form validation
+// const [validated, setValidated] = useState(false);
+// success honeypot
+// const [success, setSuccess] = useState(false);
 
-  const handleSubmit = (e) => {
-    const form = e.currentTarget;
-    e.preventDefault();
-    if (form.checkValidity() === false) {
-      e.stopPropagation();
-    } else {
-      setValidated(true);
-      setSuccess(true);
-    }
-  };
+// const handleSubmit = (e) => {
+//   const form = e.currentTarget;
+//   e.preventDefault();
+//   if (form.checkValidity() === false) {
+//     e.stopPropagation();
+//   } else {
+//     setValidated(true);
+//     setSuccess(true);
+//   }
+// };
 
-  // const navigate = useNavigate();
+// const navigate = useNavigate();
 
-  return (
-    <>
-      <p className="contact-btn m-0 p-0" onClick={handleShow}>
-        Contact
-      </p>
+// return (
+//   <>
+//     <p className="contact-btn m-0 p-0" onClick={handleShow}>
+//       Contact
+//     </p>
 
-      <Modal show={show} onHide={handleClose} size="lg" centered className="text-dark">
-        <Container>
-          <Modal.Header className="d-flex justify-content-center">
-            <Modal.Title>Contact Me</Modal.Title>
-            <CloseButton variant="dark" aria-label="Close" onClick={handleClose} />
-          </Modal.Header>
+//     <Modal show={show} onHide={handleClose} size="lg" centered className="text-dark">
+//       <Container>
+//         <Modal.Header className="d-flex justify-content-center">
+//           <Modal.Title>Contact Me</Modal.Title>
+//           <CloseButton variant="dark" aria-label="Close" onClick={handleClose} />
+//         </Modal.Header>
 
-          <Modal.Body className="modal-lg" id="modal-body">
-            <p className="lead">
-              Hi! Fill out the form below to send a message directly to my personal email.
-            </p>
-            <Form
-              noValidate
-              validated={validated}
-              onSubmit={handleSubmit}
-              id="contact-form"
-              name="contact"
-              method="POST"
-              // netlify
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              data-netlify-recaptcha="true"
-              // action={() => navigate('contact-success')}
-            >
-              {/* bot field */}
-              <input type="hidden" name="form-name" value="contact" />
-              <Form.Control type="hidden" id="bot-field" name="bot-field" />
-              {/* default email subject */}
-              <Form.Control
+//         <Modal.Body className="modal-lg" id="modal-body">
+//           <p className="lead">
+//             Hi! Fill out the form below to send a message directly to my personal email.
+//           </p>
+//           <Form
+//             noValidate
+//             validated={validated}
+//             onSubmit={handleSubmit}
+//             id="contact-form"
+//             name="contact"
+//             method="POST"
+// netlify
+// data-netlify="true"
+// data-netlify-honeypot="bot-field"
+// data-netlify-recaptcha="true"
+// action={() => navigate('contact-success')}>
+{
+  /* bot field */
+}
+{
+  /* <input type="hidden" name="form-name" value="contact" />
+              <Form.Control type="hidden" id="bot-field" name="bot-field" /> */
+}
+{
+  /* default email subject */
+}
+{
+  /* <Form.Control
                 type="hidden"
                 name="subject"
                 defaultValue="Contact message from justintsugranes.com"
-              />
-              {/* name */}
-              <Form.Group>
+              /> */
+}
+{
+  /* name */
+}
+{
+  /* <Form.Group>
                 <FloatingLabel controlId="floatingName" label="Name" className="mb-3 mt-3">
                   <Form.Control
                     type="text"
@@ -81,9 +91,13 @@ const ContactModal = () => {
                     Please enter your name.
                   </Form.Control.Feedback>
                 </FloatingLabel>
-              </Form.Group>
-              {/* email */}
-              <Form.Group className="form-floating mb-3 mt-3">
+              </Form.Group> */
+}
+{
+  /* email */
+}
+{
+  /* <Form.Group className="form-floating mb-3 mt-3">
                 <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
                   <Form.Control
                     type="email"
@@ -97,9 +111,13 @@ const ContactModal = () => {
                     Please enter your email.
                   </Form.Control.Feedback>
                 </FloatingLabel>
-              </Form.Group>
-              {/* message */}
-              <Form.Group className="form-floating mt-3 mb-3">
+              </Form.Group> */
+}
+{
+  /* message */
+}
+{
+  /* <Form.Group className="form-floating mt-3 mb-3">
                 <FloatingLabel controlId="floatingTextarea" label="Enter message">
                   <Form.Control
                     as="textarea"
@@ -114,9 +132,12 @@ const ContactModal = () => {
                     Please enter your message.
                   </Form.Control.Feedback>
                 </FloatingLabel>
-              </Form.Group>
-              {/* submit */}
-              <Button
+              </Form.Group> */
+}
+{
+  /* submit */
+}
+/* <Button
                 onClick={handleSubmit}
                 type="submit"
                 variant="dark"
@@ -132,6 +153,85 @@ const ContactModal = () => {
           </Modal.Body>
         </Container>
       </Modal>
+    </>
+  );
+}; }*/
+
+const ContactModal = () => {
+  // form validation
+  const [validated, setValidated] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      setValidated(true);
+    } else {
+      setSuccess(true);
+    }
+  };
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      {!success ? (
+        <>
+          <p className="contact-btn m-0 p-0" onClick={handleShow}>
+            Contact
+          </p>
+
+          <Modal show={show} onHide={handleClose} size="lg" centered className="text-dark">
+            <Container>
+              <Modal.Header className="d-flex justify-content-center">
+                <Modal.Title>Contact Me</Modal.Title>
+                <CloseButton variant="dark" aria-label="Close" onClick={handleClose} />
+              </Modal.Header>
+
+              <Modal.Body className="modal-lg" id="modal-body">
+                <p className="lead">
+                  Hi! Fill out the form below to send a message directly to my personal email.
+                </p>
+                <Form
+                  noValidate
+                  validated={validated}
+                  onSubmit={handleSubmit}
+                  id="contact-form"
+                  name="contact"
+                  method="POST"
+                  // netlify
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                  data-netlify-recaptcha="true">
+                  {/* form content goes here */}
+                </Form>
+              </Modal.Body>
+              <Modal.Footer className="d-flex justify-content-center">
+                <Button type="submit" variant="primary">
+                  Send
+                </Button>
+              </Modal.Footer>
+            </Container>
+          </Modal>
+        </>
+      ) : (
+        <Modal show={show} onHide={handleClose} size="lg" centered className="text-dark">
+          <Container>
+            <Modal.Header className="d-flex justify-content-center">
+              <Modal.Title>Success!</Modal.Title>
+              <CloseButton variant="dark" aria-label="Close" onClick={handleClose} />
+            </Modal.Header>
+
+            <Modal.Body className="modal-lg" id="modal-body">
+              <p className="lead">
+                Your message was successfully sent! Thank you for contacting me.
+              </p>
+            </Modal.Body>
+          </Container>
+        </Modal>
+      )}
     </>
   );
 };
