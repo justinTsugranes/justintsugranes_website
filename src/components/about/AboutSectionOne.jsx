29d624d8467100662fc7561ef9cf013998b33f5e
justintsugranes <justintsugranes@proton.me>
2023-01-04 21:20:35 -0500

    removed unused classnames

--- src/components/about/AboutSectionOne.jsx
+++ src/components/about/AboutSectionOne.jsx
@@ -5,20 +5,30 @@ import { motion } from 'framer-motion';
 
 // Section01: large title header
 const AboutSectionOne = () => {
+  // Use ref hook to reference the Row element
   const ref = useRef(null);
 
   return (
+    // Row element with ref prop set to ref hook
     <Row className="section-container p-md-5" ref={ref}>
+      {/* Wrapping the content in a motion.div element to apply motion variants */}
       <motion.div
+        // Applying the staggerContainer motion object
         variants={staggerContainer}
+        // Initial state is "hidden"
         initial="hidden"
+        // When the component comes into view, the state changes to "show"
         whileInView="show"
+        // When the component comes into view (when it takes up 25% of the viewport), the state change occurs
         viewport={{ once: true, amount: 0.25 }}
+        // Ref prop set to ref hook
         ref={ref}>
-        <div className="flex justify-center items-center flex-col relative z-10">
+        <div className="justify-center relative z-10">
+          {/* Applying the textVariant motion object to the h1 element */}
           <motion.h1 variants={textVariant(1.1)}>about</motion.h1>
-          <motion.div variants={textVariant(1.2)} className="text-gradient px-5">
-            <h1>Justin</h1>
+          {/* Applying the textVariant motion object to the div element */}
+          <motion.div variants={textVariant(1.2)} className="px-5">
+            <h1 className="text-gradient">Justin</h1>
           </motion.div>
         </div>
       </motion.div>
