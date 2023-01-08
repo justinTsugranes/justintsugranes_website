ac558c689c65d29c85220192c3d74e5052ffbf30
justintsugranes <justintsugranes@proton.me>
2023-01-08 08:03:31 -0500

    add new sanity project

---
+++ sanity.js
@@ -0,0 +1,43 @@
+export default {
+  name: 'project',
+  type: 'document',
+  title: 'Project',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description',
+      description: 'A brief overview of the project'
+    },
+    {
+      name: 'technologies',
+      type: 'array',
+      title: 'Technologies',
+      description: 'The technologies used in this project',
+      of: [{ type: 'string' }]
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+      description: 'A screenshot or preview of the project'
+    },
+    {
+      name: 'demoLink',
+      type: 'url',
+      title: 'Demo Link',
+      description: 'A link to the live demo of the project'
+    },
+    {
+      name: 'repoLink',
+      type: 'url',
+      title: 'Repository Link',
+      description: 'A link to the repository for this project'
+    }
+  ]
+};
