7cb6eb15f607b4e092db377847ae06fb1e8570fc
justintsugranes <87552618+justinTsugranes@users.noreply.github.com>
2023-07-08 04:48:18 -0400

    use ternary for footer errors

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,46 +11,48 @@ const Footer = () => {
     year: 'numeric',
   })
 
-  if (error) {
-    return <Error message={error.message} />
-  }
-
   return (
     <Row className="py-5">
       <Col className="mx-auto text-center text-light">
-        <h2 className="text-uppercase mb-3 h3 text-gradient">
-          {data?.[0]?.title}
-        </h2>
-        <p className="mb-5">{data?.[0]?.subtitle}</p>
-        <h3 className="text-uppercase h4 text-gradient">Get In Touch</h3>
+        {error ? (
+          <Error message={error.message} />
+        ) : (
+          <>
+            <h2 className="text-uppercase mb-3 h3 text-gradient">
+              {data?.[0]?.title}
+            </h2>
+            <p className="mb-5">{data?.[0]?.subtitle}</p>
+            <h3 className="text-uppercase h4 text-gradient">Get In Touch</h3>
 
-        <Row className="d-flex justify-content-around py-4 pb-0">
-          <div className="mb-4">
-            {socialMedia.map((social) => (
-              <motion.div
-                key={social.id}
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-                className="btn btn-link btn-floating btn-lg m-1 icon"
-              >
-                <a
-                  href={social.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  role="button"
-                  className="text-light btn-outline-light"
-                >
-                  {social.icon}
-                </a>
-              </motion.div>
-            ))}
-          </div>
-        </Row>
+            <Row className="d-flex justify-content-around py-4 pb-0">
+              <div className="mb-4">
+                {socialMedia.map((social) => (
+                  <motion.div
+                    key={social.id}
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+                    className="btn btn-link btn-floating btn-lg m-1 icon"
+                  >
+                    <a
+                      href={social.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      role="button"
+                      className="text-light btn-outline-light"
+                    >
+                      {social.icon}
+                    </a>
+                  </motion.div>
+                ))}
+              </div>
+            </Row>
 
-        <p className="text-secondary fs-6 mb-0">
-          &copy; {currentYear} misfitDodo Media
-        </p>
+            <p className="text-secondary fs-6 mb-0">
+              &copy; {currentYear} misfitDodo Media
+            </p>
+          </>
+        )}
       </Col>
     </Row>
   )
