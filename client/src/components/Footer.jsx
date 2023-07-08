898ebe9869cabeb088e1122b5028548f96647a18
justintsugranes <87552618+justinTsugranes@users.noreply.github.com>
2023-07-08 04:13:49 -0400

    refactor footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,14 @@
-import * as React from 'react'
+import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { socialMedia } from '../constants'
 import { motion } from 'framer-motion'
 import { useFetchData } from '../hooks'
 
-const currentYear = new Date().toLocaleDateString('en-US', {
-  year: 'numeric',
-})
-
 const Footer = () => {
   const { data, error } = useFetchData(`*[title == "Justin Tsugranes"]`)
+  const currentYear = new Date().toLocaleDateString('en-US', {
+    year: 'numeric',
+  })
 
   if (error) {
     return <p>{error.message}</p>
@@ -19,9 +18,9 @@ const Footer = () => {
     <Row className="py-5">
       <Col className="mx-auto text-center text-light">
         <h2 className="text-uppercase mb-3 h3 text-gradient">
-          {data[0]?.title}
+          {data?.[0]?.title}
         </h2>
-        <p className="mb-5">{data[0]?.subtitle}</p>
+        <p className="mb-5">{data?.[0]?.subtitle}</p>
         <h3 className="text-uppercase h4 text-gradient">Get In Touch</h3>
 
         <Row className="d-flex justify-content-around py-4 pb-0">
@@ -48,11 +47,9 @@ const Footer = () => {
           </div>
         </Row>
 
-        <Row>
-          <p className="text-secondary fs-6 mb-0">
-            Copyright © {currentYear} misfitDodo Media
-          </p>
-        </Row>
+        <p className="text-secondary fs-6 mb-0">
+          &copy; {currentYear} misfitDodo Media
+        </p>
       </Col>
     </Row>
   )
