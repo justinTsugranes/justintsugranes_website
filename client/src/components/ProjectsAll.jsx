2e47f6f2e4127172e8eb0368400216ca491f3b8d
justintsugranes <87552618+justinTsugranes@users.noreply.github.com>
2023-07-08 04:53:48 -0400

    add ternary to projects

--- client/src/components/ProjectsAll.jsx
+++ client/src/components/ProjectsAll.jsx
@@ -5,13 +5,11 @@ import { ProjectCard, Error } from './'
 const ProjectsAll = () => {
   const { data, error } = useFetchData(`*[_type == "project"]`)
 
-  if (error) {
-    return <Error message={error.message} />
-  }
-
   return (
     <Row className="d-flex justify-content-evenly row-cols-1 row-cols-md-3 row-cols-lg-5 gap-5 mb-3">
-      {data ? (
+      {error ? (
+        <Error message={error.message} />
+      ) : data && data.length ? (
         data.map((project, index) => (
           <Col key={project._id}>
             <ProjectCard {...project} index={index} projectData={data} />
